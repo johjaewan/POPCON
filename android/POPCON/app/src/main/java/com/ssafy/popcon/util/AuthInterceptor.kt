09c5e768ad93b7844d17d60fb0e9b9a129fcf1e5
wkdqhrud4870 <jangbg4870@gmail.com>
2023-02-06 00:37:03 +0900

    S08P12D201-231 @jjjjyj4870 jwt수정 중
    Former-commit-id: fc7007ae1dcf42662b1b8318c2b60ea3bf90f095

--- android/POPCON/app/src/main/java/com/ssafy/popcon/util/AuthInterceptor.kt
+++ android/POPCON/app/src/main/java/com/ssafy/popcon/util/AuthInterceptor.kt
@@ -2,18 +2,75 @@ package com.ssafy.popcon.util
 
 import android.util.Log
 import com.ssafy.popcon.config.ApplicationClass
+import com.ssafy.popcon.repository.auth.AuthRemoteDataSource
+import com.ssafy.popcon.repository.auth.AuthRepository
+import kotlinx.coroutines.runBlocking
 import okhttp3.Interceptor
 import okhttp3.Response
 
+private const val TAG = "AuthInterceptor_###"
 class AuthInterceptor : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
+        val request = chain.request()
+
         var req =
-            chain.request().newBuilder().addHeader(
+            request.newBuilder().addHeader(
                 "Authorization",
                 "Bearer ${ApplicationClass.sharedPreferencesUtil.accessToken ?: ""}"
             ).build()
         Log.d("TAG", "auth intercept: ${ApplicationClass.sharedPreferencesUtil.accessToken}")
 
-        return chain.proceed(req)
+        var response = chain.proceed(req)
+        when(response.code){
+            401 -> {
+                val authRepo = AuthRepository(AuthRemoteDataSource(RetrofitUtil.authService))
+                runBlocking {
+                    val res = authRepo.refreshToken(ApplicationClass.sharedPreferencesUtil.refreshToken!!)
+                    ApplicationClass.sharedPreferencesUtil.accessToken = res.acessToken
+                    ApplicationClass.sharedPreferencesUtil.refreshToken = res.refreshToekn
+
+                    Log.d(TAG, "intercept: ${ApplicationClass.sharedPreferencesUtil.accessToken}")
+                    req =
+                        request.newBuilder().addHeader(
+                            "Authorization",
+                            "Bearer ${ApplicationClass.sharedPreferencesUtil.refreshToken ?: ""}"
+                        ).build()
+                    response = chain.proceed(req)
+                }
+            } 500 -> {
+            val authRepo = AuthRepository(AuthRemoteDataSource(RetrofitUtil.authService))
+            runBlocking {
+                val res = authRepo.refreshToken(ApplicationClass.sharedPreferencesUtil.refreshToken!!)
+                ApplicationClass.sharedPreferencesUtil.accessToken = res.acessToken
+                ApplicationClass.sharedPreferencesUtil.refreshToken = res.refreshToekn
+
+                Log.d(TAG, "intercept: ${ApplicationClass.sharedPreferencesUtil.accessToken}")
+                req =
+                    request.newBuilder().addHeader(
+                        "Authorization",
+                        "Bearer ${ApplicationClass.sharedPreferencesUtil.refreshToken ?: ""}"
+                    ).build()
+                response = chain.proceed(req)
+            }
+        }
+            403 -> {
+                val authRepo = AuthRepository(AuthRemoteDataSource(RetrofitUtil.authService))
+                runBlocking {
+                    val res = authRepo.refreshToken(ApplicationClass.sharedPreferencesUtil.refreshToken!!)
+                    ApplicationClass.sharedPreferencesUtil.accessToken = res.acessToken
+                    ApplicationClass.sharedPreferencesUtil.refreshToken = res.refreshToekn
+
+                    Log.d(TAG, "intercept?: ${ApplicationClass.sharedPreferencesUtil.accessToken}")
+                    req =
+                        request.newBuilder().addHeader(
+                            "Authorization",
+                            "Bearer ${ApplicationClass.sharedPreferencesUtil.refreshToken ?: ""}"
+                        ).build()
+                    response = chain.proceed(req)
+                }
+            }
+        }
+
+        return response
     }
 }
